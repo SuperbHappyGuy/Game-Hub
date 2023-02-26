94b4610c4c6bb14e4ba2f9965fdda538fe8be3d2
Brandon <brandon_noel@live.ca>
2023-02-26 00:26:40 -0330

    got rid of unused areas of website

--- template.js
+++ template.js
@@ -24,55 +24,7 @@ let {title, excerpt, body, tags} = post;
         <script src="http://dreamlo.com/lb/63c44c768f40bb08f437072f/js" type="text/javascript"></script>
       </ul>
       `
-let adventureHtml = `
-                      <p>adventure</p>
-                      <img id= "construction" src="./Imgs/construction.png">
-                    `
-let puzzleHtml = `
-                    <p>puzzle</p>
-                    <img id= "construction" src="./Imgs/construction.png">
-                 `
 
-let actionHtml = `
-                    <p>action</p>
-                    <img id= "construction" src="./Imgs/construction.png">
-                 `
-let multiplayerHtml = `
-                        <p>multiplayer</p>
-                        <img id= "construction" src="./Imgs/construction.png">
-                      `
-let boardGameHtml = `
-                      <p>board game</p>
-                      <img id= "construction" src="./Imgs/construction.png">
-                    `
-let pointClickHtml = `
-                        <p>point & click</p>
-                        <img id= "construction" src="./Imgs/construction.png">
-                     `
-let visualNovelHtml = `
-                        <p>visual novel</p>
-                        <img id= "construction" src="./Imgs/construction.png">
-                      `
-let arcadeHtml = `
-                    <p>arcade</p>
-                    <img id= "construction" src="./Imgs/construction.png">
-                 `
-
-let login = `
-              <div>
-                <input placeholder= "Username"> 
-                <input placeholder= "Password"> 
-                <button class= "sideBtn">Login</button>
-             </div>
-
-             <div>
-                <input placeholder= "Email"> 
-                <input placeholder= "Username"> 
-                <input placeholder= "Password">
-                <input placeholder= "Confirm Password"> 
-                <button class= "sideBtn">Join</button>
-             </div>
-             `
 //Game HTML
 let picross = `
               <div id= "header">
@@ -90,100 +42,15 @@ let picross = `
               <div id="container"></div>
               <div id="leaderBoard"></div>
               `
-            
-let rockPS = `
-              <div id= "Title">
-              <h1>Rock! Paper! Sicssors!</h1>
-              </div>
 
-              <div id= "main">
-              <div>
-                <h4>Opponent</h4>
-              </div>
-
-              <script id= "logic" type="text/javascript" src="./Rock-Paper-Scissor/index.js"></script>
-              <p id= "game"></p>
-
-              <div>
-                <button class= "btn" type= "button" onclick="test()" id= "rock">Rock</button>
-                <button class= "btn" onclick="test()" type= "button" id= "paper">Paper</button>
-                <button class= "btn" type = "button" onclick= "test()" id = "scissors">Sicssors</button>
-                
-                <h4>You</h4>
-              </div>
-              </div>
-              `
-
-let breeding = `
-                <button id="egg" onclick='hatch()'>Hatch</button>
-                <div id="field">
-                    <ul id="pen">
-
-                    </ul>
-                </div>
-
-                <div id="barn">
-                    <div id="breeding" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                    <button id="hatch" onclick="breeding()">Breed</button>
-                </div>
-
-                <div id="overflow" ondrop="drop(event)" ondragover="allowDrop(event)">Pen</div>
-                <button onclick="compactor()" id="compactor">Compactor</button>
-                <div id="trash" ondrop="drop(event)" ondragover="allowDrop(event)">Trash</div>          
-                `
 function navigate(clicked_id) {
     var x = document.getElementById("main");
   if(clicked_id === undefined || clicked_id == "home") {
     x.innerHTML = mainHtml;
   }
-  if(clicked_id == "adventure")
-  {
-    x.innerHTML = adventureHtml;
-  }
-  if(clicked_id == "puzzle")
-  {
-    x.innerHTML = puzzleHtml;
-  }
-  if(clicked_id == "action")
-  {
-    x.innerHTML = actionHtml;
-  }
-  if(clicked_id == "multiplayer")
-  {
-    x.innerHTML = multiplayerHtml;
-  }
-  if(clicked_id == "boardGame")
-  {
-    x.innerHTML = boardGameHtml;
-  }
-  if(clicked_id == "pointClick")
-  {
-    x.innerHTML = pointClickHtml;
-  }
-  if(clicked_id == "visualNovel")
-  {
-    x.innerHTML = visualNovelHtml;
-  }
-  if(clicked_id == "arcade")
-  {
-    x.innerHTML = arcadeHtml;
-  }
-  
-  if(clicked_id == "login")
-  {
-    x.innerHTML = login;
-  }
 
   if(clicked_id == "picross")
   {
     x.innerHTML = picross;
-  }
-  if(clicked_id == "rockPS")
-  {
-    x.innerHTML = rockPS;
-  }
-  if(clicked_id == "breeding")
-  {
-    x.innerHTML = breeding;
   }
 }
