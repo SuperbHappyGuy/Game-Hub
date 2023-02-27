d6ca3a2409be8d6729a305af3a9fb82300f211e1
Brandon <brandon_noel@live.ca>
2023-02-27 17:08:10 -0330

    removed some buttons and moved logo text

--- template.js
+++ template.js
@@ -1,27 +1,8 @@
-let post = {
-    title: 'JavaScript Template Literals',
-    excerpt: 'Introduction to JavaScript template literals in ES6',
-    body: 'Content of the post will be here...',
-    tags: ['es6', 'template literals', 'javascript']
-};
-
-let {title, excerpt, body, tags} = post;
-
 //Menu HTML
   let mainHtml = `
         <ul id= "list">
         <li><a href= "https://github.com/SuperbHappyGuy/RPG-Text-Game">RPG Text Game (W.I.P.)</a></li>
-        <li><button id= "rockPS" onclick= "navigate(this.id)">Rock Paper Scissors (W.I.P)</button></li>
         <li><button id= "picross" onclick= "navigate(this.id)">Picross Randomizer(W.I.P.)</button></li>
-        <li><button id= "breeding" onclick= "navigate(this.id)">Breeding Game(W.I.P.)</button></li>
-
-        <style type="text/css">
-        .dreamloLBTable { border-collapse:collapse;text-align:center;width: 200px; }
-        .dreamloLBTable th { border-bottom: 1px solid #EEEEEE;font-weight:bold;margin:0;padding:4px; }
-        .dreamloLBTable td { border-bottom: 1px solid#EEEEEE;margin:0;padding:4px; }
-        </style>
-
-        <script src="http://dreamlo.com/lb/63c44c768f40bb08f437072f/js" type="text/javascript"></script>
       </ul>
       `
 
