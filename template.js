a6cb4557624334afe9d0bdcbc52d0e4a11404942
Brandon <brandon_noel@live.ca>
2023-03-05 14:47:50 -0330

    added a dropdown menu to picross

--- template.js
+++ template.js
@@ -10,9 +10,29 @@ let picross = `
               <h1>Picross</h1>
               </div>
 
-              <div>
-                Gird Size: <input id= "input" type="text" name="size">
-                <input id="btn" type="button" onclick="gridStart()" value="Submit"><br>
+              <div class="dropdown">
+                <button class="dropbtn">Puzzle Size</button>
+                <div class="dropdown-content">
+                  <button onclick="gridStart(2)" class="dropDownOptions">2x2</button>
+                  <button onclick="gridStart(3)" class="dropDownOptions">3x3</button>
+                  <button onclick="gridStart(4)" class="dropDownOptions">4x4</button>
+                  <button onclick="gridStart(5)" class="dropDownOptions">5x5</button>
+                  <button onclick="gridStart(6)" class="dropDownOptions">6x6</button>
+                  <button onclick="gridStart(7)" class="dropDownOptions">7x7</button>
+                  <button onclick="gridStart(8)" class="dropDownOptions">8x8</button>
+                  <button onclick="gridStart(9)" class="dropDownOptions">9x9</button>
+                  <button onclick="gridStart(10)" class="dropDownOptions">10x10</button>
+                  <button onclick="gridStart(11)" class="dropDownOptions">11x11</button>
+                  <button onclick="gridStart(12)" class="dropDownOptions">12x12</button>
+                  <button onclick="gridStart(13)" class="dropDownOptions">13x13</button>
+                  <button onclick="gridStart(14)" class="dropDownOptions">14x14</button>
+                  <button onclick="gridStart(15)" class="dropDownOptions">15x15</button>
+                  <button onclick="gridStart(16)" class="dropDownOptions">16x16</button>
+                  <button onclick="gridStart(17)" class="dropDownOptions">17x17</button>
+                  <button onclick="gridStart(18)" class="dropDownOptions">18x18</button>
+                  <button onclick="gridStart(19)" class="dropDownOptions">19x19</button>
+                  <button onclick="gridStart(20)" class="dropDownOptions">20x20</button>
+                </div>
               </div>
 
               <div id="time"></div>
@@ -31,5 +51,6 @@ function navigate(clicked_id) {
   if(clicked_id == "picross")
   {
     x.innerHTML = picross;
+    //gridStart(5);
   }
 }
